beffb797f80b0bd3d7b2537e8c4466658fda6aec
zeroway <M_O_Bz@163.com>
2016-08-10 16:47:35 +0000

    batch download pictures from a url

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # batch_down
 batch download pictures form a url with python
+
+Usage:
+	假设需要批量下载的图片URL如下所示:  
+	http://example_target_url/01.jp  
+	http://example_target_url/02.jpg  
+	...  
+	http://example_target_url/50.jpg  
+	批量下载这50张图片:  
+	Start value:1  
+	Stop value:50  
+	Enter the url prefix: 'http://example_target_url/'  
+	Enter the save dir: '/home/pictures/'  
---
+++ batch_down.py
@@ -0,0 +1,42 @@
+import urllib2  
+from urllib2 import HTTPError, URLError  
+import math  
+import sys  
+import time  
+  
+print "The start time: "+time.ctime()  
+x = input("Start value:")  
+y = input("Stop value:")  
+k = y-x  
+target_url_prefix = input("Enter the url prefix: ")
+save_local_dir = input("Enter the save dir: ")
+for i in range(x, y+1):  
+    if i < 10:
+        url = str(target_url_prefix) + '0' + str(i) + '.jpg'  
+    else:
+        url = str(target_url_prefix) + str(i) + '.jpg'  
+    print url
+    req = urllib2.Request(url)  
+    req.add_header("User-agent", "Mozilla 5.10")  
+    name = str(save_local_dir) + str(i) + '.jpg'  
+    print name
+    try:  
+        conn = urllib2.urlopen(req)  
+        f = open(name, 'wb')  
+        f.write(conn.read())  
+        f.close()  
+        if k < 100:  
+            sys.stdout.write("Computing: [%s%s] %.2f%%\r" % ('#' * (i-x) , '-' * (y-i), (i-x)*100/k))  
+            sys.stdout.flush()  
+            time.sleep(0.01)  
+        else:  
+            sys.stdout.write("Computing: [%s%s] %.2f%% \r" % ('#' * ((i-x)/(k/50)) , '-' * ((y-i)/(k/50)), (i-x)*100.0/k))  
+            sys.stdout.flush()  
+            time.sleep(0.01)  
+    except HTTPError ,e:  
+        i = i+1  
+    except URLError, e:  
+        print "The server\'s  something is wrong!"  
+        break  
+print "\nThe finished time: "+time.ctime()  
+print 'pic saved'  
